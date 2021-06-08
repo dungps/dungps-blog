a2272e4ed34b0b614292e6cf75d0406fc1694dbf
Dzung Pham <dungpham@beeketing.net>
2021-06-08 07:59:19 +0700

    feat(home): update style for home

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,9 +1,20 @@
-import React from "react"
+import React, {useEffect, Fragment} from "react"
+import imgBg from "../../assets/img/img_parallax.jpeg"
+import style from "./style.module.scss"
 
 const Home = () => {
-    return (
-        <h1>Home</h1>
-    )
+	useEffect(() => {
+		document.body.style.overflow = "hidden"
+	})
+
+	return (
+		<Fragment>
+			<div className={style.bgImg}>
+				<img className={style.img} src={imgBg} alt="home-img"/>
+			</div>
+			<div className={style.bgOverlay} />
+		</Fragment>
+	)
 }
 
 export default Home
