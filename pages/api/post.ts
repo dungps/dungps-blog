a6d638e6c3ea721fd59a68cd844a5d122f770ef0
Kevin Pham <hi@dungps.com>
2021-06-27 14:32:20 +0700

    fix(api): remove sample data

--- pages/api/post.ts
+++ pages/api/post.ts
@@ -29,33 +29,6 @@ export default async (req: NextApiRequest, res: NextApiResponse<Response>) => {
 
         let convertData = JSON.parse(Buffer.from(response.data.content, 'base64').toString('ascii'));
 
-        convertData = [
-            {
-                'title': 'Test post',
-                'slug': 'test-post',
-                'category': {
-                    slug: 'ahihi',
-                    label: 'ahihi'
-                },
-                'tags': ['test', 'test-2'],
-                'feature_image': 'https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg',
-                'link': '_contents/test-post.md',
-                content: '# Hello'
-            },
-            {
-                'title': 'Test post',
-                'slug': 'test-post-2',
-                'category': {
-                    slug: 'ahihi-1',
-                    label: 'ahihi 1'
-                },
-                'tags': ['test-1', 'test-2'],
-                'feature_image': 'https://helpx.adobe.com/content/dam/help/en/photoshop/using/convert-color-image-black-white/jcr_content/main-pars/before_and_after/image-before/Landscape-Color.jpg',
-                'link': '_contents/test-post.md',
-                'content': '# Hello'
-            },
-        ];
-
         if (category && category.length) {
             convertData = convertData.filter((o: Post) => o.category.slug === category);
         }
@@ -68,8 +41,8 @@ export default async (req: NextApiRequest, res: NextApiResponse<Response>) => {
             const index = convertData.findIndex((o: Post) => o.slug === slug);
 
             if (index > -1 && convertData[index] && convertData[index].link) {
-                // const postRes = await githubRequest.get<GithubResponse>(`/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${convertData[index].link}`);
-                // convertData[index].content = Buffer.from(postRes.data.content, 'base64').toString('ascii');
+                const postRes = await githubRequest.get<GithubResponse>(`/repos/${GITHUB_OWNER}/${GITHUB_REPO}/contents/${convertData[index].link}`);
+                convertData[index].content = Buffer.from(postRes.data.content, 'base64').toString('ascii');
                 return res.status(200).json({
                     success: true,
                     data: convertData[index],
